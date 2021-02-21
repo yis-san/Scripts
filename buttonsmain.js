fb9b27a8606dc14eec132f3b884806f34b4c6eaa
Bartek <75677928+yis-san@users.noreply.github.com>
2021-02-21 14:09:38 +0100

    Add files via upload

---
+++ buttonsmain.js
@@ -0,0 +1,24 @@
+function rtask1() {
+    window.open("subsites/rtask1.html")
+}
+function rtask2() {
+    window.open("subsites/rtask2.html")
+}
+function rtask3() {
+    window.open("subsites/rtask3.html")
+}
+function rtask4() {
+    window.open("subsites/rtask4.html")
+}
+function rtask5() {
+    window.open("subsites/rtask5.html")
+}
+function rtask6() {
+    window.open("subsites/rtask6.html")
+}
+function rtask7() {
+    window.open("subsites/rtask7.html")
+}
+function rtask8() {
+    window.open("subsites/rtask8.html")
+}
